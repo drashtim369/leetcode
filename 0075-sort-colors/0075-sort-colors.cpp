0db30032588e1d8d3dec019ec2990c4002d33fdf
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-01-26 13:16:24 +0530

    Time: 6 ms (0.78%), Space: 11.5 MB (96.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low = 0;
-        int mid = 0;
-        int high = nums.size() - 1;
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                swap(nums[mid], nums[low]);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else {
-                swap(nums[mid], nums[high]);
-                high--;
-            }
+        int n = nums.size(), cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) cnt0++;
+            else if (nums[i] == 1) cnt1++;
+            else cnt2++;
         }
+        cout << cnt0 << ' ' << cnt1 << ' ' << cnt2 << endl;;
+        nums.clear();
+        for (int i = 0; i < cnt0; i++) nums.emplace_back(0);
+        for (int i = 0; i < cnt1; i++) nums.emplace_back(1);
+        for (int i = 0; i < cnt2; i++) nums.emplace_back(2);
+        for (int i = 0; i < nums.size(); i++) cout << nums[i] << " ";
     }
 };
