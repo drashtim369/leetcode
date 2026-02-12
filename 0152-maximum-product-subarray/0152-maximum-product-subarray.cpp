a1203382128c20be5e69958ca6817be132d3518b
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-02-12 21:46:09 +0530

    Time: 0 ms (100%), Space: 17.7 MB (59.09%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,18 +4,13 @@ public:
         int mx = nums[0];
         int mn = nums[0];
         int global = nums[0];
-
         for (int i = 1; i < nums.size(); i++) {
             int x = nums[i];
-
             if (x < 0) swap(mx, mn);
-
             mx = max(x, mx * x);
             mn = min(x, mn * x);
-
             global = max(global, mx);
         }
-
         return global;
     }
 };
