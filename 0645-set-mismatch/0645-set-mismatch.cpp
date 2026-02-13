519d5f469c14f1506354c7061bf3088370979b1b
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-02-13 15:07:11 +0530

    Time: 0 ms (100%), Space: 25 MB (97.8%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int xr = 0, zero = 0, one = 0; 
+        long long n = nums.size();
+        for (int i = 0; i < n; i++) {
+            xr = xr ^ nums[i];
+            xr = xr ^ (i+1);
+        }
+        int bitNo = 0;
+        while (true) {
+            if ((xr & (1<<bitNo)) != 0) {
+                break;
+            }
+            bitNo++;
+        }
+        for (int i = 0; i < n; i++) {
+            if ((nums[i] & (1<<bitNo)) != 0) {
+                one = one ^ nums[i];
+            } else {
+                zero = zero ^ nums[i];
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if ((i & (1<<bitNo)) != 0) {
+                one = one ^ i;
+            } else {
+                zero = zero ^ i;
+            }
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == zero) cnt++;
+        }
+        if (cnt == 2) return{zero, one};
+        return{one, zero};
+    }
+};
