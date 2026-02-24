1a058c6c8df26826ccd726281fa345cc95353d39
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-02-24 14:31:25 +0530

    Time: 7 ms (90.9%), Space: 35 MB (86.67%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -5,7 +5,7 @@ public:
         int max_capacity = accumulate(weights.begin(), weights.end(), 0);
         while (min_capacity <= max_capacity) {
             int mid_capacity = min_capacity + (max_capacity - min_capacity) / 2;
-            int d = 0, sum = 0;
+            int d = 1, sum = 0;
             for (auto i : weights) {
                 sum += i;
                 if (sum > mid_capacity) {
@@ -13,8 +13,7 @@ public:
                     sum = i;
                 }
             }
-            
-            if (d < days) {
+            if (d <= days) {
                 max_capacity = mid_capacity - 1;
             } else {
                 min_capacity = mid_capacity + 1;
