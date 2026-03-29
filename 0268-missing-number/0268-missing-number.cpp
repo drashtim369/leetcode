938e4c545b2d4de9d48af85b5f11a2abc682a530
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-03-29 13:16:18 +0530

    Time: 0 ms (100%), Space: 21.8 MB (37.59%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        int x1 = 0, x2 = 0;
         int n = nums.size();
-        int sum = (n * (n + 1)) / 2;
-        int missum = 0;
-        for (auto i = 0; i < nums.size(); i++) {
-            missum += nums[i];
+        for (int i = 0; i < n; i++) {
+            x1 = x1 ^ nums[i];
+            x2 = x2 ^ i;
         }
-        cout << sum << " " << missum << endl;
-        return sum - missum;
+        return (x1^x2^n);
     }
 };
