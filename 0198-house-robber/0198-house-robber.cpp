6dc2f685454bfe9541230ebacbe4badad25e7754
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-04-05 14:46:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int prev1 = 0, prev2 = 0;
+        int prev1 = 0, prev2 = 0, curr;
         for (int i = 0; i < nums.size(); i++) {
-            int curr = max(prev1, prev2 + nums[i]);
+            curr = max(prev1, prev2 + nums[i]);
             prev2 = prev1;
             prev1 = curr;
         }
