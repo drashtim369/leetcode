53f990ef371d5e1477dbce7221699e683e096c17
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-04-12 11:42:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,19 +1,14 @@
-class Solution {
-
-    void bt(int[] nums, boolean[] used, List<Integer> curr, List<List<Integer>> res) {
-
+class Solution {    
+    List<List<Integer>> res = new ArrayList<>();
+    void bt(int[] nums, boolean[] used, List<Integer> curr) {
         if (curr.size() == nums.length) {
             res.add(new ArrayList<>(curr));
             return;
         }
-
         for (int i = 0; i < nums.length; i++) {
-
             if (used[i]) continue;
-
             used[i] = true;
             curr.add(nums[i]);
-
             bt(nums, used, curr, res);
 
             // 🔙 backtrack
@@ -23,8 +18,7 @@ class Solution {
     }
 
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        bt(nums, new boolean[nums.length], new ArrayList<>(), res);
+        bt(nums, new boolean[nums.length], new ArrayList<>());
         return res;
     }
 }
