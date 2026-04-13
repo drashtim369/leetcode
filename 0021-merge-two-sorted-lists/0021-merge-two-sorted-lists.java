4288e601b2f342020afc18d224499f30d18d8e04
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-04-13 14:36:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -2,14 +2,14 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
         ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
+        ListNode temp = dummy.next;
 
         while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
-                temp.next = list1;
+                temp = list1;
                 list1 = list1.next;
             } else {
-                temp.next = list2;
+                temp = list2;
                 list2 = list2.next;
             }
             temp = temp.next;
@@ -22,6 +22,6 @@ class Solution {
             temp.next = list2;
         }
 
-        return dummy.next;
+        return temp;
     }
 }
