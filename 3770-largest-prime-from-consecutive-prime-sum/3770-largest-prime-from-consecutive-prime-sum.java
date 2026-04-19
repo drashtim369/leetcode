1958ddea00b63f1898cfff924d3d8b39bd587f6b
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-04-19 09:59:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.java
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.java
@@ -2,7 +2,6 @@ class Solution {
     public int largestPrime(int n) {
         int sum = 0;
         int ans = 0;
-
         for (int i = 2; sum + i <= n; i++) {
             if (isPrime(i)) {
                 sum += i;
@@ -12,13 +11,10 @@ class Solution {
                 }
             }
         }
-
         return ans;
     }
-
-    private boolean isPrime(int num) {
+    boolean isPrime(int num) {
         if (num < 2) return false;
-
         for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) return false;
         }
