30bdc6320380d05c14a52674ad7743da4dc33bac
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-04-19 09:58:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.java
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.java
@@ -1,47 +1,27 @@
 class Solution {
     public int largestPrime(int n) {
-        List<Integer> primes = getPrimeList(n);
-        int sum = 0, mx = 0; // FIXED
+        int sum = 0;
+        int ans = 0;
 
-        for (int it : primes) {
-            sum += it;
-            if (sum > n) break; // optimization
+        for (int i = 2; sum + i <= n; i++) {
+            if (isPrime(i)) {
+                sum += i;
 
-            if (checkPrime(sum)) {
-                mx = Math.max(mx, sum);
+                if (isPrime(sum)) {
+                    ans = sum;
+                }
             }
         }
-        return mx;
+
+        return ans;
     }
 
-    boolean checkPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
         }
         return true;
-    }
-
-    public static List<Integer> getPrimeList(int n) {
-        boolean[] isPrime = new boolean[n + 1];
-        Arrays.fill(isPrime, true);
-
-        if (n >= 0) isPrime[0] = false;
-        if (n >= 1) isPrime[1] = false;
-
-        for (int p = 2; p * p <= n; p++) {
-            if (isPrime[p]) {
-                for (int i = p * p; i <= n; i += p)
-                    isPrime[i] = false;
-            }
-        }
-
-        List<Integer> primeList = new ArrayList<>();
-        for (int i = 2; i <= n; i++) {
-            if (isPrime[i]) {
-                primeList.add(i);
-            }
-        }
-        return primeList;
     }
 }
