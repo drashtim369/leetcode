d82bde6547569aa15ab1a0a227a39fbc83a99149
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-06-06 15:55:47 +0530

    Time: 8 ms (58.89%), Space: 44.8 MB (46.57%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,16 +3,15 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        int left = 0, right = 0, maxChild = 0;
+        int left = 0, right = 0;
         while (left < g.size() && right < s.size()) {
             if (g[left] <= s[right]) {
                 right++;
                 left++;
-                maxChild++;
             } else {
                 right++;
             }
         }
-        return maxChild;
+        return left;
     }
 };
