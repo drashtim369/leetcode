602543245dba24ec2d4179275d77fe3e452197e4
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-06-19 19:23:45 +0530

    Time: 4 ms (38.86%), Space: 43.4 MB (25.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,19 @@
-import java.util.*;
-
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            // opening brackets
-            if (c == '(' || c == '[' || c == '{') {
-                st.push(c);
-            } 
-            else {
-                // ❌ no opening to match
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                st.push(s.charAt(i));
+            } else {
                 if (st.isEmpty()) return false;
-
-                char top = st.pop();
-
-                // ❌ mismatch
-                if ((c == ')' && top != '(') ||
-                    (c == ']' && top != '[') ||
-                    (c == '}' && top != '{')) {
+                int top = st.pop();
+                if (s.charAt(i) == ')' && top != '(' ||
+                    s.charAt(i) == ']' && top != '[' ||
+                    s.charAt(i) == '}' && top != '{') {
                     return false;
                 }
             }
         }
-
-        return st.isEmpty();
+        return st.isEmpty() ? true : false;
     }
 }
