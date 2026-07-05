2f27a39375ba58fe708f0cc4b3503d4ddd690b66
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-07-05 22:20:59 +0530

    Time: 0 ms (100%), Space: 23.7 MB (64.11%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-    vector<int> res;
-    int candidateOne = 0, candidateTwo = 0, cnt1 = 0, cnt2 = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        if (cnt1 == 0) {
-            candidateOne = nums[i];
-            cnt1 = 1;
-        } else if (cnt2 == 0 && nums[i] != candidateOne) {
-            candidateTwo = nums[i];
-            cnt2 = 1;
-        } else if (nums[i] == candidateOne) {
-            cnt1++;
-        } else if (nums[i] == candidateTwo) {
-            cnt2++;
-        } else {
-            cnt1--;
-            cnt2--;
+        vector<int> res;
+        int c1, c2, cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int el = nums[i];
+            if (el != c2 && cnt1 == 0) {
+                c1 = el;
+                cnt1++;
+            } else if (el != c1 && cnt2 == 0) {
+                c2 = el;
+                cnt2++;
+            } else if (el == c1) {
+                cnt1++;
+            } else if (el == c2) {
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
         }
-    }
-    int cnt3 = 0, cnt4 = 0;
-    for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] == candidateOne) {
-            cnt3++;
-        } else if (nums[i] == candidateTwo) {
-            cnt4++;
-        } else {
-            continue;
-        }
-    }
-    if (cnt3 > (nums.size())/3) res.emplace_back(candidateOne);
-    if (cnt4 > (nums.size())/3) res.emplace_back(candidateTwo);
-    return res;
+        int cnt3 = 0, cnt4 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == c1) cnt3++;
+            if (nums[i] == c2) cnt4++;
+        }  
+        if (cnt3 > (nums.size() / 3)) res.push_back(c1);
+        if (cnt4 > (nums.size() / 3)) res.push_back(c2);
+        return res;
     }
 };
