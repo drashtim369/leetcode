3a1aa4796271d00f79aa32691679364a7f661a9f
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-07-05 23:41:50 +0530

    Time: 26 ms (26.25%), Space: 17.5 MB (55.11%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n = nums.size();
         vector<vector<int>> res;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.size(); i++) {
             if (i > 0 && nums[i] == nums[i-1]) continue;
-            for (int j = i+1; j < n; j++) {
-                if (j > i+1 && nums[j] == nums[j-1]) continue;
-                int k = j+1, m = n-1;
-                long long target2 = (long long) target - nums[i] - nums[j]; 
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) continue;
+                int k = j + 1, m = nums.size() - 1;
                 while (k < m) {
-                    long long sum = (long long) nums[k] + nums[m];
-                    if (sum < target2) {
-                        k++;
-                    } else if (sum > target2) {
-                        m--;
-                    } else {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[m];
+                    if (sum == target) {
                         res.push_back({nums[i], nums[j], nums[k], nums[m]});
+                        while (k < m && nums[k] == nums[k+1]) k++;
+                        while (k < m && nums[m] == nums[m-1]) m--;
+                        k++; m--; 
+                    } else if (sum < target) {
                         k++;
+                    } else {
                         m--;
-                        while (k < m && nums[k] == nums[k-1]) k++;
-                        while (k < m && nums[m] == nums[m+1]) m--;
                     }
                 }
             }
