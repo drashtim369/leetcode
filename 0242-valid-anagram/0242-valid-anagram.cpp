848f4ff43b07e37735bc371f113c6c01ba920e1b
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-07-06 18:11:16 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if (s.size() != t.size()) return false;
-        vector<int>freq(128,0); //Create a 128 sized array filled with zeroes -> [0,0,0...0];
+        vector<int> freq(999);
         for (int i = 0; i < s.size(); i++) {
             freq[s[i]]++;
             freq[t[i]]--;
         }
-        for (int num : freq) {
-            if (num != 0) return false;
+        for(auto it : freq) {
+            if (it != 0) {
+                return false;
+            }
         }
         return true;
     }
