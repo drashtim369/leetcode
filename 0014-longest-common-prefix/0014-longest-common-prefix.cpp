e6e7a15178035c13b6ffa5da339a7d9e90b0d629
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-07-06 18:03:53 +0530

    Time: 0 ms (100%), Space: 11.9 MB (58.14%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -5,9 +5,7 @@ public:
         for (int i = 0; i < strs[0].size(); i++) {
             char ch = strs[0][i];
             for (int j = 1; j < strs.size(); j++) {
-                if (i >= strs[j].size() || ch != strs[j][i]) {
-                    return ans;
-                }
+                if (ch != strs[j][i] || i >= strs[j].size()) return ans;
             }
             ans += ch;
         }
