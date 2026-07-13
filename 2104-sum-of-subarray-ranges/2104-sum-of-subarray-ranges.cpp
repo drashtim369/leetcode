186ab41fffc6d7af5159fd0beab05ce4ee9b39d0
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-07-13 15:44:00 +0530

    Time: 3 ms (73.49%), Space: 15.8 MB (57.67%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> leftSmallest(n,-1);
+        vector<int> leftLargest(n,-1);
+        vector<int> rightSmallest(n,n);
+        vector<int> rightLargest(n,n);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                leftSmallest[i] = st.top();
+            }
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] <= nums[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                leftLargest[i] = st.top();
+            }
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] > nums[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                rightSmallest[i] = st.top();
+            }
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] < nums[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                rightLargest[i] = st.top();
+            }
+            st.push(i);
+        }
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            long long maxContribution = 1LL * nums[i] * (i - leftLargest[i]) * (rightLargest[i] - i);
+            long long minContribution = 1LL * nums[i] * (i - leftSmallest[i]) * (rightSmallest[i] - i);    
+            ans += maxContribution - minContribution;
+        }
+        return ans;
+    }
+};
