d35cac6d036f1fd7dd35ba96bc848215d4b6110b
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-07-31 23:15:05 +0530

    Time: 0 ms (100%), Space: 9.6 MB (63.97%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -5,8 +5,11 @@ public:
         for (int i = 0; i < numRows; i++) {
             res[i].resize(i+1);
             for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) res[i][j] = 1;
-                else res[i][j] = res[i-1][j-1] + res[i-1][j];
+                if (j == 0 || j == i) {
+                    res[i][j] = 1;
+                } else {
+                    res[i][j] = res[i-1][j-1] + res[i-1][j];
+                }
             }
         }
         return res;
