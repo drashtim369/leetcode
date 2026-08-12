51083a914d8e72488ef271b17d2b3b10a57d9acb
drashtim369 <141033487+drashtim369@users.noreply.github.com>
2026-08-12 18:32:06 +0530

    Time: 5 ms (54.43%), Space: 20.1 MB (24.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& p) {
-        vector<bool> vis(numCourses, false);
-        vector<bool> pathvis(numCourses, false);
-        vector<vector<int>> arr(numCourses);
-        for (auto it : p) {
-    arr[it[1]].push_back(it[0]);
-}
-        for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (dfs(i, vis, pathvis, arr)) {
-                    return false;
+    bool canFinish(int n, vector<vector<int>>& p) {
+        vector<int> indegree(n, 0);
+        vector<vector<int>> arr(n);
+        for (auto i : p) {
+            arr[i[1]].push_back(i[0]);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int node : arr[i]) {
+                indegree[node]++;
+            }
+        }
+        queue<int> q; int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) q.push(i);
+        }
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+            cnt++;
+            for (int node : arr[x]) {
+                indegree[node]--;
+                if (indegree[node] == 0) {
+                    q.push(node);
                 }
             }
         }
-        return true;
-    }
-    bool dfs(int node, vector<bool>& vis, vector<bool>& pathvis, vector<vector<int>>& arr) {
-        vis[node] = true;
-        pathvis[node] = true;
-        for (int adj : arr[node]) {
-            if (!vis[adj]) {
-                if (dfs(adj, vis, pathvis, arr)) {
-                    return true;
-                }
-            } else if (pathvis[adj]) {
-                return true;
-            }
-        }
-        pathvis[node] = false;
-        return false;
+        return cnt == n;
     }
 };
